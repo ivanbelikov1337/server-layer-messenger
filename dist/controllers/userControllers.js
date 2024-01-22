204616a45b7efdaa3620f6661d12fe67d07db72b
ivanbelikov1337 <120462639+ivanbelikov1337@users.noreply.github.com>
2024-01-22 22:14:47 +0200

    Update userControllers.js

--- dist/controllers/userControllers.js
+++ dist/controllers/userControllers.js
@@ -43,10 +43,10 @@ exports.allUsers = (0, express_async_handler_1.default)((req, res) => __awaiter(
 // }));
 
 exports.updateUser = (0, express_async_handler_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { _id, id } = req.body;
+    const { _id, idAppwrite } = req.body;
     const filter = { _id: _id };
     yield (0, db_1.connectDB)();
-    const user = yield userModel_1.default.findOneAndUpdate(filter, { id: id }, { new: true });
+    const user = yield userModel_1.default.findOneAndUpdate(filter, { idAppwrite: idAppwrite }, { new: true });
     yield user.save();
     console.log(user);
     if (user) {
